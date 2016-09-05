6b460d470aa929df068ac6319f1e0a055b51602c
ShankarShastri <shastri.shankar9@gmail.com>
2016-09-05 17:55:21 +0530

    Added Typed.js feauture
    Added Typed.js feauture

--- core.js
+++ core.js
@@ -1,7 +1,6 @@
 var app = angular.module('Angular_Calc', []);
 
 app.controller('angularCalcController', ['$scope', '$http', function($scope, $http) {
-    console.log("Entered Controller");
     $scope.Output = '0';
     $scope.OutputCalc = '0';
     $scope.flagCalc = false;
@@ -19,10 +18,8 @@ app.controller('angularCalcController', ['$scope', '$http', function($scope, $ht
             }
 
             $scope.Output = $scope.OutputCalc;
-            console.log($scope.Output);
         } else {
-            console.log(val);
-            console.log(isNaN(val));
+
             if (isNaN(val)) {
                 $scope.Output = $scope.Output + val;
 
@@ -35,9 +32,9 @@ app.controller('angularCalcController', ['$scope', '$http', function($scope, $ht
     };
 
     $scope.calcOutput = function() {
-      var temp = 0;
+        var temp = 0;
         try {
-             temp = eval($scope.Output);
+            temp = eval($scope.Output);
         } catch (e) {
             return;
         }
@@ -47,11 +44,81 @@ app.controller('angularCalcController', ['$scope', '$http', function($scope, $ht
         }
         $scope.OutputCalc = '0';
         $scope.flagCalc = true;
-        console.log($scope.Output);
     };
 
     $scope.clearOutput = function() {
         $scope.Output = '0';
         $scope.OutputCalc = '0';
     };
+}]);
+
+
+
+
+
+//directive taken from typed.js
+app.directive('typewrite', ['$timeout', function($timeout) {
+    function linkFunction(scope, iElement, iAttrs) {
+        var timer = null,
+            initialDelay = iAttrs.initialDelay ? getTypeDelay(iAttrs.initialDelay) : 200,
+            typeDelay = iAttrs.typeDelay ? getTypeDelay(iAttrs.typeDelay) : 200,
+            blinkDelay = iAttrs.blinkDelay ? getAnimationDelay(iAttrs.blinkDelay) : false,
+            cursor = iAttrs.cursor ? iAttrs.cursor : '|',
+            blinkCursor = iAttrs.blinkCursor ? iAttrs.blinkCursor === "true" : true,
+            auxStyle;
+        if (iAttrs.text) {
+            timer = $timeout(function() {
+                updateIt(iElement, 0, iAttrs.text);
+            }, initialDelay);
+        }
+
+        function updateIt(element, i, text) {
+            if (i <= text.length) {
+                element.html(text.substring(0, i) + cursor);
+                i++;
+                timer = $timeout(function() {
+                    updateIt(iElement, i, text);
+                }, typeDelay);
+                return;
+            } else {
+                if (blinkCursor) {
+                    if (blinkDelay) {
+                        auxStyle = '-webkit-animation:blink-it steps(1) ' + blinkDelay + ' infinite;-moz-animation:blink-it steps(1) ' + blinkDelay + ' infinite ' +
+                            '-ms-animation:blink-it steps(1) ' + blinkDelay + ' infinite;-o-animation:blink-it steps(1) ' + blinkDelay + ' infinite; ' +
+                            'animation:blink-it steps(1) ' + blinkDelay + ' infinite;';
+                        element.html(text.substring(0, i) + '<span class="blink" style="' + auxStyle + '">' + cursor + '</span>');
+                    } else {
+                        element.html(text.substring(0, i) + '<span class="blink">' + cursor + '</span>');
+                    }
+                } else {
+                    element.html(text.substring(0, i));
+                }
+            }
+        }
+
+        function getTypeDelay(delay) {
+            if (typeof delay === 'string') {
+                return delay.charAt(delay.length - 1) === 's' ? parseInt(delay.substring(0, delay.length - 1), 10) * 1000 : +delay;
+            }
+        }
+
+        function getAnimationDelay(delay) {
+            if (typeof delay === 'string') {
+                return delay.charAt(delay.length - 1) === 's' ? delay : parseInt(delay.substring(0, delay.length - 1), 10) / 1000;
+            }
+        }
+
+        scope.$on('$destroy', function() {
+            if (timer) {
+                $timeout.cancel(timer);
+            }
+        });
+    }
+
+    return {
+        restrict: 'A',
+        link: linkFunction,
+        scope: false
+    };
+
 }]);
